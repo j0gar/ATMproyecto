62231a723b53f05e18e1c68364d889555db4f322
j0gar <53158003+j0gar@users.noreply.github.com>
2026-07-20 22:33:54 +0200

    Delete MJ-Core directory

--- MJ-Core/mjcore/core/config.lua
+++
@@ -1,8 +0,0 @@
-return {
-    version = "0.1.0",
-    monitorName = nil, -- nil = detectar automaticamente
-    textScale = 0.5,
-    title = "M&J CORE",
-    ownerLine = "Mia + J0gar",
-    refreshSeconds = 1
-}
