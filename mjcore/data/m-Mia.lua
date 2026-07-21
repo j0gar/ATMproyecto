3639bd98a675d51a867ff2dc730007f135499ff1
j0gar <53158003+j0gar@users.noreply.github.com>
2026-07-21 16:13:51 +0200

    Update m-Mia.lua

--- mjcore/data/m-Mia.lua
+++ mjcore/data/m-Mia.lua
@@ -1,9 +1,9 @@
 return {
-    "Buenos días, Mia. La base está lista.",
-    "Bienvenida, Mia. Que tengas un buen día.",
-    "Mia ha entrado en la zona. Todo bajo control.",
+    "Bienvenida mi vida, espero no te estreses mucho.",
+    "Oñaaaa te amooo.",
+    "Mia ha entrado en la zona. ¡ESCONDAN LAS ROSAS NEGRAS QUE NO LAS VEA!.",
     "Hola, Mia. Recuerda revisar tus tareas.",
     "Bienvenida de nuevo, Mia.",
-    "M&J Core te desea un buen día.",
-    "Mia detectada. Sistemas de la base operativos."
+    "M&J Core te desea un buen día Y J0GAR TE AMA CON LOCURA.",
+    "Mia detectada. La base está a tope."
 }
